3cc32f96235268e66e6d0b33cd989c24351b06bb
Keith Tate <tateke@yahoo.com>
2015-04-21 10:42:48 -0700

    Update code and variable names
    I have updated the variable names and added more comments to, hopefully,
    make it easier to read and understand what the functions are doing.

--- cachematrix.R
+++ cachematrix.R
@@ -4,41 +4,54 @@
 
 
 makeCacheMatrix <- function(x = matrix()) {
-    #Set m (local variable) to NULL (undefined object) 
-    m <- NULL
+
+    #This stores the cached inverse matrix if it exists
+    #The first time there is no cached version so the value
+    # will be NULL
+    cache <- NULL
     
-    #Create a nested function set
-    set <- function(y) {
-        #Check to see if the variables were already assigned in the parent environment
-        #If no then assign them here
-        x <<- y
-        m <<- NULL
+    #Create a nested function set to store the matrix
+    setMatrix <- function(MatrixValue) {
+        #Assign x to the matrix value that was passed in to the function
+        x <<- MatrixValue
+        # Since we have a new matrix value we need to make sure that cache is NULL
+        cache <<- NULL
     }
     
-    get <- function() x
-    setSolve <- function(Solve) m <<- Solve
-    getSolve <- function() m
-    list(set = set, get = get,
-         setSolve = setSolve,
-         getSolve = getSolve)
+    # Return the matrix 
+    getMatrix <- function() x
+    
+    # Cache the inverser of the matrix, using Solve
+    SolveInverse <- function(Solve) {
+        cache <<- Solve
+    }
+    
+    # get the cached inverse matrix
+    GetInverse <- function() cache
+    
+    # Return a list of each function
+    list(setMatrix = setMatrix, getMatrix = getMatrix, SolveInverse = SolveInverse, GetInverse = GetInverse)
 }
 
 
-cacheSolve <- function(x, ...) {
-    # Declare a local variable inside this funciton and set it to the passed in
-    # matrix subsetted by the getSolve function
-    m <- x$getSolve()
-    # If m is not null then we already have the data cached and we can simply
-    # return m which is the cached inversed matrix
-    if(!is.null(m)) {
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve retrieves the inverse from the cache.
+cacheSolve <- function(y, ...) {
+    # Get the cached inverse matrix that was created in makeCacheMatrix, if it exists
+    InverseMatrix <- y$GetInverse()
+    
+    # If there is a cached value for the matrix then return it along with a message to let the user know that the value was cached.
+    if(!is.null(InverseMatrix)) {
         message("getting cached data")
-        return(m)
+        return(InverseMatrix)
     }
     
     # The data is not cached so now we need to inverse the passed in matrix by
-    # using solve
-    data <- x$get()
-    m <- solve(data, ...)
-    x$setSolve(m)
-    m
+    # using solve and then cache it
+    data <- y$getMatrix()
+    InverseMatrix <- solve(data, ...)
+    y$SolveInverse(InverseMatrix)
+
+    # return the newly cached inverse of the matrix
+    InverseMatrix
 }
