a610e00e30eb09424e3df0abc2d3255ca97fb6f3
Keith Tate <tateke@yahoo.com>
2015-04-21 09:51:32 -0700

    Updated cachematrix.R to my solution
    I have added my code in place of the stub that was there

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions help to allieviate the costly computation required to inverse a matrix.
+## If the matrix has already been cached then it will retrieve the cache instead instead of doing
+## the inversion again
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    #Set m (local variable) to NULL (undefined object) 
+    m <- NULL
+    
+    #Create a nested function set
+    set <- function(y) {
+        #Check to see if the variables were already assigned in the parent environment
+        #If no then assign them here
+        x <<- y
+        m <<- NULL
+    }
+    
+    get <- function() x
+    setSolve <- function(Solve) m <<- Solve
+    getSolve <- function() m
+    list(set = set, get = get,
+         setSolve = setSolve,
+         getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # Declare a local variable inside this funciton and set it to the passed in
+    # matrix subsetted by the getSolve function
+    m <- x$getSolve()
+    # If m is not null then we already have the data cached and we can simply
+    # return m which is the cached inversed matrix
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    
+    # The data is not cached so now we need to inverse the passed in matrix by
+    # using solve
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setSolve(m)
+    m
 }
